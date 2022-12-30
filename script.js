eb35090d37ade2a22b87404c62fd9f47bcfd5be3
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2022-12-30 21:22:13 +0530

    module test

---
+++ script.js
@@ -0,0 +1,13 @@
+function shown(){
+    var name1=document.getElementById('name1')
+    var name2=document.getElementById('name2')
+    var name3=document.getElementById('name3')
+
+    if(name1.value ==='' | name2.value === '' | name3.value ===''){
+
+    }else{
+        document.getElementById('msg').style.opacity='1';
+    }
+
+
+}
